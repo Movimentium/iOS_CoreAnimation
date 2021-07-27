9038a5f821ba994e52da874521dc0a82ee44d277
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2021-07-27 15:01:03 +0200

    func addFireworkEmitter done

--- iOS_CoreAnimation/FireWorksVC.swift
+++ iOS_CoreAnimation/FireWorksVC.swift
@@ -11,8 +11,17 @@ class FireWorksVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+    }
+    
+    
+    // MARK: - Particle emitter
+    func addFireworkEmitter() {
+        let emitter = CAEmitterLayer()
+        emitter.frame = view.bounds
+        emitter.emitterPosition = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+        emitter.emitterSize = CGSize(width: 150, height: 150)
+        emitter.emitterShape = .point
+        view.layer.addSublayer(emitter)
     }
     
 
--- iOS_CoreAnimation/MainVC.swift
+++ iOS_CoreAnimation/MainVC.swift
@@ -7,16 +7,41 @@
 
 import UIKit
 
-class MainVC: UIViewController {
-    
+class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
     @IBOutlet weak var table: UITableView!
-    
+    let arrVCs = ["FireWorksVC"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
     }
     
+    
+    func setupUI() {
+        table.rowHeight = 60
+        table.dataSource = self
+        table.delegate = self
+    }
 
-
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrVCs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let strVC = arrVCs[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "idCell", for: indexPath)
+        cell.textLabel?.text = strVC
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        table.deselectRow(at: indexPath, animated: true)
+        let strVC = arrVCs[indexPath.row]
+        performSegue(withIdentifier: "to" + strVC, sender: nil)
+    }
+    
 }
